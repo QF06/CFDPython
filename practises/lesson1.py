ec612e1de81ee5e5e9dfb7b95fb295ddb9b22b36
qingfeng <liqingfeng06@gmail.com>
2018-06-23 00:32:20 +0200

    propagation of a wave

--- practises/lesson1.py
+++ practises/lesson1.py
@@ -1,12 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-nx = 41
-dx = 2/(nx-1)
-nt = 25
-dt = .025
+nx = 41 # 长度上采样的个数
+dx = 2/(nx-1) # 计算所关心的长度范围
+nt = 25 # 时间上采样的个数
+dt = 10/(nt-1) # 计算所关心的时间范围
+c = 0.05 # 假设波速
 u = np.ones(nx)
-u[int(.5/dx):int(1/dx)]=2
-l=np.linspace(0,2,nx)
+u[int(.5/dx):int(1/dx)] = 2 #初始值
+l = np.linspace(0,2,nx)
+
+un = np.ones(nx)
+for n in range(nt):
+    un = u.copy()
+    for i in range(nx):
+        u[i] = un[i] - c*dt/dx*(un[i]-un[i-1])
+
 plt.plot(l,u)
 plt.show()
